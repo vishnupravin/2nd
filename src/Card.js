1057d804e1e32388d040b62ea0ddb282ec47452e
vishnupravin <vicchunu@gmail.com>
2022-10-22 00:50:20 +0530

    yes

--- src/Card.js
+++ src/Card.js
@@ -7,17 +7,17 @@ function Card({ cardItems, removeFromcart }) {
             cardItems.length === 0 ? <h3>No Cart Items</h3> :
                 <>
                     <h3>Cart</h3>
-                    <ol class="list-group list-group-numbered">
+                    <ol className="list-group list-group-numbered">
                         {
                             cardItems.map((item) => {
-                                return <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div class="fw-bold">{item.name}</div>
+                                return <li className="list-group-item d-flex justify-content-between align-items-start">
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">{item.name}</div>
                                         <h3>Rs : {item.price}</h3>
                                     </div>
                                     <button onClick={() => {
                                         removeFromcart(item)
-                                    }} class="badge bg-primary rounded-pill">X</button>
+                                    }} className="badge bg-primary rounded-pill">X</button>
                                 </li>
                             })
                         }
